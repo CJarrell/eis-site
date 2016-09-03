5674587f0564e1a818f5453f5168fa03f81c00de
CJarrell <colby.jarrell@icloud.com>
2016-09-03 10:22:27 -0400

    eis-site v2 first commit, header is coming together

---
+++ js/scroll.js
@@ -0,0 +1,23 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+// delay animations on brand type and nav bar
+$(function() {
+  setTimeout(function () {
+    $(".eis-brand").addClass('slideDown');
+  }, 700);
+  setTimeout(function () {
+    $(".eis-nav-container").addClass('slideUp');
+  }, 1400);
+
+});
+
